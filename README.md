c5a1ad1ec10c6014713a140eaf9e34ca44bae250
Philip Goddinger <philip.goddinger@gmail.com>
2023-12-21 22:14:07 +0100

    added cv_results_ info

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Randomized and Grid Search CV Parameter Visualization
 
+Analysis of the results of randomized and grid search cross-validation for hyperparameter tuning. (cv_results_)
--- routes/start.py
+++ routes/start.py
@@ -3,6 +3,9 @@ import streamlit as st
 
 def start():
     st.title("Welcome to the ML Model Evaluation App")
+    st.write(
+        "This app is designed to help you evaluate your ML model based on the cv_results_ of your model"
+    )
     st.write("Please upload your data to get started")
     st.write("You can upload your data on the left hand side")
     st.write(
