f9c03eda1beedb0d359ef7a1d8de39c4506c02d9
Philip Goddinger <philip.goddinger@gmail.com>
2023-12-21 20:04:31 +0100

    init commit

---
+++ pages/upload.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
